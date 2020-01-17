e44745ba50734906593d1df7b0be787fe25c4b5c
LH <vk2gzz@ieee.org>
2020-01-18 08:34:15 +1100

    random

---
+++ ImportExport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GZZLogger
+{
+    class ImportExport
+    {
+        private Database database;
+        private Settings settings;
+        public ImportExport(Database database, Settings settings)
+        {
+            this.database = database;
+            this.settings = settings;
+        }
+
+        //public void Export
+
+
+    }
+}
